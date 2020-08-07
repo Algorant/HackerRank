337a5d6099dfcf8d436291192b9c5c898e59bb1b
Skantastico <Skantastico@gmail.com>
2020-08-07 13:50:41 -0400

    Added Day 1 readme, pdf, solution

---
+++ 30_days_of_code/d01_data_types/README.md
@@ -0,0 +1,7 @@
+This challenge gives a basic overview of data types (strings, integers, floats)
+
+[Link to Challenge](https://www.hackerrank.com/challenges/30-data-types/problem)
+
+[Link to PDF](./30-data-types-English.pdf)
+
+[My Solution](./data_types.py)
---
+++ 30_days_of_code/d01_data_types/data_types.py
@@ -0,0 +1,34 @@
+# The sample code includes these variables
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Declare second integer, double, and String variables.
+
+j = int(12)
+
+f = float(4.0)
+
+k = str('is the best place to learn and practice coding!')
+
+# Read and save an integer, double, and String to your variables.
+
+j = int(input())
+
+f = float(input())
+
+k = str(input())
+
+# Print the sum of both integer variables on a new line.
+
+print(int(i + j))
+
+# Print the sum of the double variables on a new line.
+
+print(float(d + f))
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+
+print(str(s + k))
