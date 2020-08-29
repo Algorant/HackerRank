93bbc37ad1e334b0c74b021817bc199d8e81b5c5
Skantastico <Skantastico@gmail.com>
2020-08-29 12:58:32 -0400

    completed challenge day 24

---
+++ 30_days_of_code/d24_more_linked_lists/README.md
@@ -0,0 +1,7 @@
+This challenge asks the user to create a function that removes dupliates in a given list. It iterates in pointer-style through the data of size integer N (number of nodes).
+
+Link to [Challenge]https://www.hackerrank.com/challenges/30-linked-list-deletion/problem)
+
+Link to [PDF](./more_linked_lists.pdf)
+
+Link to [Solution](./more_linked.py)
---
+++ 30_days_of_code/d24_more_linked_lists/more_linked.py
@@ -0,0 +1,39 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+class Solution:
+    def insert(self,head,data):
+            p = Node(data)
+            if head==None:
+                head=p
+            elif head.next==None:
+                head.next=p
+            else:
+                start=head
+                while(start.next!=None):
+                    start=start.next
+                start.next=p
+            return head
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def removeDuplicates(self,head):
+        # this is the part of the challenge user completes. It begins blank
+            if head:
+                t = self.removeDuplicates(head.next)
+                if t and head.data == t.data:
+                    head.next = t.next
+            return head
+
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)
+head=mylist.removeDuplicates(head)
+mylist.display(head);
