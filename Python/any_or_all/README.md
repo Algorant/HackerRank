43ab9f519e2d87530532bae56cb56c092226f485
Skantastico <Skantastico@gmail.com>
2020-10-19 09:41:07 -0400

    completed any or all challenge

---
+++ Python/any_or_all/README.md
@@ -0,0 +1,8 @@
+Given a list of integers, if it is positive, check if it is a palindromic integer.
+
+
+Link to [Challenge](https://www.hackerrank.com/challenges/any-or-all/problem)
+
+Link to [PDF](./any-or-all.pdf)
+
+Link to [Solution](./anyall.py)
---
+++ Python/any_or_all/anyall.py
@@ -0,0 +1,16 @@
+'''
+Given a list of integers, if it is positive,
+check if it is a palindromic integer.
+'''
+
+N = int(input())
+integers = input().split()
+
+if all(int(i) >= 0 for i in integers):
+    if any(num == num[-1:] for num in integers):
+        print("True")
+    else:
+        print("False")
+
+else:
+    print("False")
