f606eeeb3770d2d6c74952fa8a0b66b52ba52bd2
Skantastico <Skantastico@gmail.com>
2020-11-02 23:17:19 -0500

    completed roman numeral challenge

---
+++ Python/roman_numerals/README.md
@@ -0,0 +1,7 @@
+Given a string, if it is a valid roman numeral print True, else print False.
+
+Link to [Challenge](https://www.hackerrank.com/challenges/validate-a-roman-number/problem)
+
+Link to [PDF](./validating-roman-numerals.pdf)
+
+Link to [Solution](./roman.py)
---
+++ Python/roman_numerals/roman.py
@@ -0,0 +1,14 @@
+'''
+Given a string, if it is a valid roman numeral print True,
+else print False.
+'''
+
+thousand = "(?:(M){0,3})?"
+hundred  = "(?:(D?(C){0,3})|(CM)|(CD))?"
+ten      = "(?:(L?(X){0,3})|(XC)|(XL))?"
+unit     = "(?:(V?(I){0,3})|(IX)|(IV))?"
+
+regex_pattern = r"^" + thousand + hundred + ten + unit + "$"
+
+import re
+print(str(bool(re.match(regex_pattern, input()))))
