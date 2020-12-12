ab6727d08802cd7e0da3eaed375a779f633daa7c
Skantastico <Skantastico@gmail.com>
2020-12-12 10:18:24 -0500

    completed default argument challenge all 115 finished!

---
+++ Python/default_arguments/README.md
@@ -0,0 +1,7 @@
+Debug the given code so that it successfully executes all provided test files.
+
+Link to [Challenge](https://www.hackerrank.com/challenges/default-arguments/problem)
+
+Link to [PDF](default-arguments.pdf)
+
+Link to [Solution](./default.py)
---
+++ Python/default_arguments/default.py
@@ -0,0 +1,41 @@
+'''
+Debug the given code so that it successfully executes all provided test files.
+'''
+
+
+# In line 28, def print_from_stream has an issue in it, the default argument
+# called stream=EvenStream() is only called once, and should be called every
+# time. Therefore we add the init line in 29 to make it do this.
+
+class EvenStream(object):
+    def __init__(self):
+        self.current = 0
+
+    def get_next(self):
+        to_return = self.current
+        self.current += 2
+        return to_return
+
+class OddStream(object):
+    def __init__(self):
+        self.current = 1
+
+    def get_next(self):
+        to_return = self.current
+        self.current += 2
+        return to_return
+
+def print_from_stream(n, stream=EvenStream()):
+    stream.__init__() #alternatively can do if statement here for stream=None
+    for _ in range(n):
+        print(stream.get_next())
+
+
+queries = int(input())
+for _ in range(queries):
+    stream_name, n = input().split()
+    n = int(n)
+    if stream_name == "even":
+        print_from_stream(n)
+    else:
+        print_from_stream(n, OddStream())
